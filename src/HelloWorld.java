ce9cc309aad5fd349d60d58eddfa6681fb4a3156
Daniel Name <daniel.v.name@gmail.com>
2022-10-09 12:57:14 -0500

    completed #10 on the java syntax exercise.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber);
+        String myString = "My name is Daniel.";
+        System.out.println(myString);
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
     }
 }
