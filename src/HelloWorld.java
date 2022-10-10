f49749ac473d126abb68f7cbd0615fe1671c06b5
Daniel Name <daniel.v.name@gmail.com>
2022-10-10 16:30:45 -0500

    completed Java syntax exercise #13

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,9 +7,20 @@ public class HelloWorld {
         System.out.println(myString);
         float myNumber = 3.14f;
         System.out.println(myNumber);
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
-        int three = (int) "three";;
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
     }
 }
