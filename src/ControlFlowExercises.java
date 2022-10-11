33968e34b64a8d10690c4b3706beee68979e1408
Daniel Name <daniel.v.name@gmail.com>
2022-10-11 12:29:52 -0500

    do while loops except for squaring

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,8 +1,24 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i < 15){
-            i++;
-        }
+//        int i = 5;
+//        while (i < 15){
+//            i++;
+//            System.out.println(i);
+//        }
+        int i = 0;
+        do {
+            i += 2;
+            System.out.println(i);
+        }while (i < 100);
+        do {
+            i -= 5;
+            System.out.println(i);
+        }while (i > -10);
+        i += 12;
+        System.out.println(i);
+//        do {
+//            i *= i;
+//            System.out.println(i);
+//        }while (i < 1000000);
     }
 }
