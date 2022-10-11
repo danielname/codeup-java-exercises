33b86cd7415d156d06934f22c31a7262bb481962
Daniel Name <daniel.v.name@gmail.com>
2022-10-11 13:03:56 -0500

    fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -20,17 +20,28 @@ public class ControlFlowExercises {
 //            i = i * i;
 //            System.out.println(i);
 //        }while (i < 50000);
-        for (int i = 5; i <= 15; i++){
-            System.out.println(i);
-        }
-        for (int i = 0; i < 100; i += 2){
-            System.out.println(i);
-        }
-        for (int i = 100; i > -15; i -= 5){
-            System.out.println(i);
-        }
-        for (int i = 2; i < 50000; i = i * i){
-            System.out.println(i);
+//        for (int i = 5; i <= 15; i++){
+//            System.out.println(i);
+//        }
+//        for (int i = 0; i < 100; i += 2){
+//            System.out.println(i);
+//        }
+//        for (int i = 100; i > -15; i -= 5){
+//            System.out.println(i);
+//        }
+//        for (int i = 2; i < 50000; i = i * i){
+//            System.out.println(i);
+//        }
+        for (int i = 1; i <= 100; i++){
+            if(i % 5 == 0 && i % 3 == 0){
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
