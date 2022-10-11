5631035390560b05ecc8d397a1ca1a23a08cb650
Daniel Name <daniel.v.name@gmail.com>
2022-10-11 12:43:55 -0500

    completed for section of control flow exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,20 +5,32 @@ public class ControlFlowExercises {
 //            i++;
 //            System.out.println(i);
 //        }
-        int i = 0;
-        do {
-            i += 2;
+//        int i = 0;
+//        do {
+//            i += 2;
+//            System.out.println(i);
+//        }while (i < 100);
+//        do {
+//            i -= 5;
+//            System.out.println(i);
+//        }while (i > -10);
+//        i += 12;
+//        System.out.println(i);
+//        do {
+//            i = i * i;
+//            System.out.println(i);
+//        }while (i < 50000);
+        for (int i = 5; i <= 15; i++){
             System.out.println(i);
-        }while (i < 100);
-        do {
-            i -= 5;
+        }
+        for (int i = 0; i < 100; i += 2){
             System.out.println(i);
-        }while (i > -10);
-        i += 12;
-        System.out.println(i);
-        do {
-            i = i * i;
+        }
+        for (int i = 100; i > -15; i -= 5){
             System.out.println(i);
-        }while (i < 50000);
+        }
+        for (int i = 2; i < 50000; i = i * i){
+            System.out.println(i);
+        }
     }
 }
