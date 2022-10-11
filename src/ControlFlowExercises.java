23a4ab3dc855930396d810de0e5a7b78eb3c3274
Daniel Name <daniel.v.name@gmail.com>
2022-10-11 14:35:34 -0500

    control flow exercise complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -43,16 +43,43 @@ public class ControlFlowExercises {
 //            } else {
 //                System.out.println(i);
 //            }
+        Scanner scanner = new Scanner(System.in);
         char answer;
-        do {Scanner scanner = new Scanner(System.in);
-        System.out.println("What number would you like to go up to? ");
-        int tableLimit = scanner.nextInt();
-        System.out.printf("Here is your table!%n%nnumber    |squared   |cubed     %n----------|----------|----------%n");
-        for (int i = 1; i <= tableLimit; i++){
-            System.out.println(i + "         |" + (i * i) + "         |" + (i * i * i));
-        }
-        System.out.println("Would you like to continue? y/n");
-        answer = scanner.next().charAt(0);
-        } while (answer == 'y');
+//        do {
+//        System.out.println("What number would you like to go up to? ");
+//        int tableLimit = scanner.nextInt();
+//        System.out.printf("Here is your table!%n%nnumber    |squared   |cubed     %n----------|----------|----------%n");
+//        for (int i = 1; i <= tableLimit; i++){
+//            System.out.println(i + "         |" + (i * i) + "         |" + (i * i * i));
+//        }
+//        System.out.println("Would you like to continue? y/n");
+//        answer = scanner.next().charAt(0);
+//        } while (answer == 'y');
+        do {
+            System.out.println("Enter a numeric grade from 0-100: ");
+            int dnNumGrade = scanner.nextInt();
+            if (dnNumGrade > 87) {
+                System.out.println("Your grade is an A!");
+                // for now i am copy/pasting, but in theory the following should be a function
+                System.out.println("Would you like to continue? y/n");
+                answer = scanner.next().charAt(0);
+            } else if (dnNumGrade > 79) {
+                System.out.println("Your grade is a B!");
+                System.out.println("Would you like to continue? y/n");
+                answer = scanner.next().charAt(0);
+            } else if (dnNumGrade > 66) {
+                System.out.println("Your grade is a C!");
+                System.out.println("Would you like to continue? y/n");
+                answer = scanner.next().charAt(0);
+            } else if (dnNumGrade > 59) {
+                System.out.println("Your grade is a D!");
+                System.out.println("Would you like to continue? y/n");
+                answer = scanner.next().charAt(0);
+            } else {
+                System.out.println("Your grade is an F!");
+                System.out.println("Would you like to continue? y/n");
+                answer = scanner.next().charAt(0);
+            }
+        }while(answer == 'y');
     }
 }
