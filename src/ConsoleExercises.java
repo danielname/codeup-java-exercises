279504ab330a70b19d0341551c1e4f97beb8c4db
Daniel Name <daniel.v.name@gmail.com>
2022-10-11 11:30:47 -0500

    completed console exercise no bonus

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,25 +1,34 @@
 import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args){
-        double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %.2f.%n",pi);
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f.%n",pi);
         Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter an integer: ");
+//        int userInput = scanner.nextInt();
+//        System.out.println(userInput);
+//
+//        System.out.print("Enter 3 words: ");
+//        String word1 = scanner.next();
+//        String word2 = scanner.next();
+//        String word3 = scanner.next();
+//        System.out.println(word2);
+//
+//        System.out.print("Enter a sentence: ");
+//        scanner.nextLine();
+//        String sentence = scanner.nextLine();
+//        System.out.println(sentence);
 
-        System.out.print("Enter an integer: ");
-        int userInput = scanner.nextInt();
-        System.out.println(userInput);
+        System.out.println("Enter the length of the classroom in feet (numbers only): ");
+        String length = scanner.nextLine();
 
-        System.out.print("Enter 3 words: ");
-        String word1 = scanner.next();
-        String word2 = scanner.next();
-        String word3 = scanner.next();
-        System.out.println(word2);
+        System.out.println("Enter the width of the classroom in feet (numbers only): ");
+        String width = scanner.nextLine();
 
-        System.out.print("Enter a sentence: ");
-        scanner.nextLine();
-        String sentence = scanner.nextLine();
-        System.out.println(sentence);
-
-
+        int parsedLength = Integer.parseInt(length);
+        int parsedWidth = Integer.parseInt(width);
+        int classArea = parsedLength * parsedWidth;
+        System.out.println(classArea);
     }
 }
