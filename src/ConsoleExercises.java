242bd1cf9fc5bb17fe764455d29c5ab73c53f81d
Daniel Name <daniel.v.name@gmail.com>
2022-10-11 10:03:53 -0500

    got scanner functional

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,11 +1,11 @@
+import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args){
         double pi = 3.14159;
         System.out.printf("The value of pi is approximately %.2f.%n",pi);
-        import java.util.Scanner;
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter an integer: ");
-        int userInput = scanner.next();
+        int userInput = scanner.nextInt();
     }
 }
