425af08f78ac3598ec4881d39bc8d4025aad76bd
Daniel Name <daniel.v.name@gmail.com>
2022-10-11 14:51:13 -0500

    completed #1 on Strings

---
+++ src/StringExercise.java
@@ -0,0 +1,14 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String dnEducation = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(dnEducation);
+        String dnEscapeChars = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(dnEscapeChars);
+        String dnC = "In windows, the main drive is usually C:\\";
+        System.out.println(dnC);
+        String dnBackslash = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(dnBackslash);
+    }
+}
