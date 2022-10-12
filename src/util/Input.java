d7f93dbb6b5b6d550345f5070e44fdd6607ced51
Daniel Name <daniel.v.name@gmail.com>
2022-10-12 14:06:31 -0500

    detup input validation exercise

---
+++ src/util/Input.java
@@ -0,0 +1,23 @@
+package util;
+
+public class Input {
+    Private scanner;
+    String getString(){
+
+    }
+    boolean yesNo(){
+
+    }
+    int getInt(int min, int max){
+
+    }
+    int getInt(){
+
+    }
+    double getDouble (double min, double max){
+
+    }
+    double getDouble (){
+
+    }
+}
