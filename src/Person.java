d3f7adb5120263e3112125829d7af83a680ecacf
Daniel Name <daniel.v.name@gmail.com>
2022-10-12 13:51:17 -0500

    completed objects basics for sure and understanding references

--- src/Person.java
+++ src/Person.java
@@ -1,23 +1,43 @@
 import java.util.Scanner;
 public class Person {
-//    public Person(String name){
-//        return //something;
-//    }
-    private String name;
-
+    public Person(String a){
+        setName(a);
+    }
+    private static String name;
     public static String getName(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name?");
-        return scanner.next();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What is your name?");
+//        String dnInputName = scanner.next();
+//        setName(dnInputName);
+//        return dnInputName;
+        return Person.name;
     }
 
-//    public void setName(String name){
-//        return name;
-//    }
+    public static void setName(String name){
+        Person.name = name;
+        //need to set person.name to any name inputted as the argument.
+    }
     public static void sayHello(){
         System.out.printf("Hello %s!%n",getName());
     }
     public static void main(String[] args) {
-        sayHello();
+//        sayHello();
+        Person person1 = new Person("John");
+        Person person2 = new Person("John");
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
     }
+
 }
