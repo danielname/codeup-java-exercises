1e47b2b26dffa6c39882b09f5d14957285b00fe3
Daniel Name <daniel.v.name@gmail.com>
2022-10-12 16:50:24 -0500

    set up circles exercise

---
+++ src/shapes/Circle.java
@@ -0,0 +1,10 @@
+package shapes;
+
+public class Circle {
+    public Circle (){
+        private radius;
+        public Circle(double radius)
+        public double getArea()
+        public double getCircumference()
+    }
+}
