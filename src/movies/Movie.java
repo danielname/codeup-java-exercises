549230573f3e67c1e6bd8385fc18467d0e45b3e6
Daniel Name <daniel.v.name@gmail.com>
2022-10-13 13:34:41 -0500

    set up movies exercise

---
+++ src/movies/Movie.java
@@ -0,0 +1,15 @@
+package movies;
+
+public class Movie {
+    public static Movie(){
+        private String name;
+        public static String setName(){
+
+        }
+        private String category;
+        public static String setCategory(){
+
+        }
+    };
+
+}
