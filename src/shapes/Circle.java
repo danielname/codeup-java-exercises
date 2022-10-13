aacd8ba5d1f1f7eccfe9f304427bd63f16985d4c
Daniel Name <daniel.v.name@gmail.com>
2022-10-13 09:13:07 -0500

    first attempt at making circle work in shapes exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -2,9 +2,15 @@ package shapes;
 
 public class Circle {
     public Circle (){
-        private radius;
-        public Circle(double radius)
-        public double getArea()
-        public double getCircumference()
+        private double radius;
+        public Circle(double radius){
+            Circle.radius = radius;
+        }
+        public double getArea(){
+            return Math.PI * Circle.radius * Circle.radius;
+        }
+        public double getCircumference(){
+            return Math.PI * Circle.radius * 2;
+        }
     }
 }
