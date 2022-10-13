406abed547d8eaaebf922c08086627ee24739aa9
Daniel Name <daniel.v.name@gmail.com>
2022-10-13 15:33:11 -0500

    mostly done with movies, need to rename array and fix loops

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -15,6 +15,37 @@ public class MoviesApplication {
                 4 - view movies in the horror category
                 5 - view movies in the scifi category""");
         byte dnUserChoice = scanner.nextByte();
+        if (dnUserChoice == 5) {
+            for (datatype item : array) {
+                if (item.setCategory().equals("scifi")) {
+                    System.out.printf("%s -- %s", item.setName(), item.setCategory());
+                }
+            }
+            } else if (dnUserChoice == 4) {
+                for (datatype item : array) {
+                    if (item.setCategory().equals("scifi")) {
+                        System.out.printf("%s -- %s", item.setName(), item.setCategory());
+                    }
+                }
+            } else if (dnUserChoice == 3) {
+                    for (datatype item : array) {
+                        if (item.setCategory().equals("scifi")) {
+                            System.out.printf("%s -- %s", item.setName(), item.setCategory());
+                        }
+                    }
+            } else if (dnUserChoice == 2) {
+                        for (datatype item : array) {
+                            if (item.setCategory().equals("scifi")) {
+                                System.out.printf("%s -- %s", item.setName(), item.setCategory());
+                            }
+                        }
+            } else if (dnUserChoice == 1) {
+            for (datatype item : array) {
+                System.out.printf("%s -- %s", item.setName(), item.setCategory());
+            }
+        }else {
 
+            }
+        }
     }
 }
