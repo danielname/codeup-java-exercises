8610504d6eaf7248d1468cf415bd8f36d201bb72
Daniel Name <daniel.v.name@gmail.com>
2022-10-14 09:30:18 -0500

    completed rectangle exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,16 @@
 package shapes;
 
 public class Rectangle {
+    protected int length;
+    protected int width;
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+    public int getArea(){
+        return this.length * this.width;
+    }
+    public int getPerimeter(){
+        return this.length * 2 + this.width * 2;
+    }
 }
