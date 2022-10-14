ea77f9c5da01eab05e4a5590ced4d32ffdb54faa
Daniel Name <daniel.v.name@gmail.com>
2022-10-14 12:03:40 -0500

    completed #1-3 of interfaces and abstract classes, the setup

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,20 +1,20 @@
 package shapes;
 
 public class Rectangle {
-    protected int length;
-    protected int width;
-    public Rectangle(int length, int width){
-        this.length = length;
-        this.width = width;
-    }
-    public Rectangle(){
-        this.length = 1;
-        this.width = 1;
-    }
-    public int getArea(){
-        return this.length * this.width;
-    }
-    public int getPerimeter(){
-        return this.length * 2 + this.width * 2;
-    }
+//    protected int length;
+//    protected int width;
+//    public Rectangle(int length, int width){
+//        this.length = length;
+//        this.width = width;
+//    }
+//    public Rectangle(){
+//        this.length = 1;
+//        this.width = 1;
+//    }
+//    public int getArea(){
+//        return this.length * this.width;
+//    }
+//    public int getPerimeter(){
+//        return this.length * 2 + this.width * 2;
+//    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,15 +2,16 @@ package shapes;
 
 public class Square extends Rectangle{
 
-    private int side;
-    public Square(int sideLength){
-        super(sideLength,sideLength);
-        this.side = sideLength;
-    }
-    public int getArea(){
-        return this.side * this.side;
-    }
-    public int getPerimeter(){
-        return this.side * 4;
-    }
+//    private int side;
+//    public Square(int sideLength){
+//        super(sideLength,sideLength);
+//        this.side = sideLength;
+//    }
+//    public int getArea(){
+//        return this.side * this.side;
+//    }
+//    public int getPerimeter(){
+//        return this.side * 4;
+//    }
+
 }
