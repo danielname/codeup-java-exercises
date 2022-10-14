304df510265dc0178581665ba3cac081a53f1760
Daniel Name <daniel.v.name@gmail.com>
2022-10-14 10:48:56 -0500

    completed inheritence and polymorphism exercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -5,5 +5,12 @@ public class Square extends Rectangle{
     private int side;
     public Square(int sideLength){
         super(sideLength,sideLength);
+        this.side = sideLength;
+    }
+    public int getArea(){
+        return this.side * this.side;
+    }
+    public int getPerimeter(){
+        return this.side * 4;
     }
 }
