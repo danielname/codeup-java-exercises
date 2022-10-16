717ed82e167ca2c68c78c0214483ac0b9050d222
Daniel Name <daniel.v.name@gmail.com>
2022-10-16 16:40:22 -0500

    completed array list exercise #1

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -6,6 +6,25 @@ public class Student {
     private String name;
     private ArrayList<Integer> grades;
     public Student(){
-        this.name =
+        this.name = "student";
+        this.grades = new ArrayList<>();
+    }
+    // returns the student's name
+    public String getName(){
+        return this.name;
+    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double sum = 0;
+        for (Integer grade : grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
     }
 }
