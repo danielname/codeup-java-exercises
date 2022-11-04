cda424dbf1b7b32ada62af114260b55f89d88872
Daniel Name <daniel.v.name@gmail.com>
2022-11-04 16:58:27 -0500

    circleapp class added

---
+++ src/fileIoPractice.java
@@ -0,0 +1,15 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class fileIoPractice {
+//    public static void main(String[] args) {
+//        try {
+//            Path p = Paths.get("src", "weywot", "java", "test.txt");
+//            Files.write(p, names, StandardOpenOption.APPEND);
+//        } catch(IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+}
---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,9 @@
+package shapes;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Circle testCircle = new Circle(13);
+        System.out.println(testCircle.getArea());
+    }
+
+}
